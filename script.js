3748326d721ee4522e14b5d83ef1b4416662aa25
atmega-p471 <64792125+atmega-p471@users.noreply.github.com>
2021-11-09 17:38:03 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -20,9 +20,25 @@ let arrPicEnemy = [];
     arrPicEnemy['left']=picEnemyLeft;
     arrPicEnemy['right']=picEnemyRight;
 
+let picLife = new Image();
+    picLife.src = "./pic/hp.png";
+let picCoin = new Image();
+    picCoin.src = "./pic/coin.png";
+
 
 ctx.canvas.width = window.innerWidth;
 ctx.canvas.height = window.innerHeight;
+
+function generateRandomPosition(pic, xMin, xMax, yMin, yMax){
+    let x = Math.random()*((xMax-pic.height)-xMin)+xMin,
+    y = Math.random()*((yMax-pic.height)-yMin)+yMin;
+    return [x, y];
+}
+//coin
+let positionCoin=[0,0];
+function newPositionCoin(){
+    positionCoin=generateRandomPosition(picCoin,0,innerWidth ,innerHeight*0.193,innerHeight*0.93)
+}
 
 function resizeImg(img, percent){
     let prop;
@@ -38,9 +54,10 @@ function resizeImg(img, percent){
 }
  let startGame = false;
  let xPlayer = 280, yPlayer = 730,
-     speedPlayer = 5,speedEnemy=50
+     speedPlayer = 5,speedEnemy=5
     navPlayer = 'right', navEnemy='left'
-     xEnemy = 1300, yEnemy = 40;
+     xEnemy = 1300, yEnemy = 40, boardPicPlayer=0, boardPicEnemy=0,
+     countLife=5;
 
 function boardPic(pic, x, y){
     let picRight, picBottom;
@@ -54,9 +71,11 @@ function draw(){
     let picEnemy = arrPicEnemy[navEnemy];
     resizeImg(picPlayer,8);
     resizeImg(picEnemy,8);
-    let boardPicPlayer = boardPic(picPlayer, xPlayer, yPlayer);
+    resizeImg(picLife,5);
+    resizeImg(picCoin,4);
+     boardPicPlayer = boardPic(picPlayer, xPlayer, yPlayer);
     console.log(boardPicPlayer)
-    let boardPicEnemy = boardPic(picEnemy, xEnemy, yEnemy);
+     boardPicEnemy = boardPic(picEnemy, xEnemy, yEnemy);
 
     function startPosition(){
         xPlayer = window.innerWidth*0.162;
@@ -67,6 +86,23 @@ function draw(){
     }
     
     function moveEnemy(){
+        function collosionEnemy(){
+            if(boardPicPlayer[0]>xEnemy && xPlayer<boardPicEnemy[0] && yPlayer<boardPicEnemy[1] && yEnemy<boardPicPlayer[1]){
+                countLife--;
+                if (countLife<= 0) {
+                    let newGame = confirm('Defeat:(\n Вернуться в меню');
+                    if(newGame){
+                        countLife=5;
+                    }else{
+                        clearTimeout(timerMoveEnemy);
+                        startPosition();
+                        return;
+                    }
+                }
+                startPosition();
+            }
+        }
+        collosionEnemy();
         let timerMoveEnemy = setTimeout(()=>{
             if(xEnemy>(xPlayer+picPlayer.width)){
                 xEnemy--;
@@ -84,13 +120,23 @@ function draw(){
     if(startGame){
         startPosition();
         moveEnemy();
+        newPositionCoin();
         startGame=false;
     }
     
     ctx.drawImage(picBackground, 0, 0, window.innerWidth, window.innerHeight);
     ctx.drawImage(picPlayer, xPlayer, yPlayer, picPlayer.width,picPlayer.height);
     ctx.drawImage(picEnemy, xEnemy, yEnemy, picEnemy.width,picEnemy.height);
+    //life
+    for(let i=0; i<countLife; i++){
+        let yLife=innerHeight*0.1;
+        let xLife=innerWidth*0.05+picLife.width*i;
+        ctx.drawImage(picLife, xLife, yLife, picLife.width,picLife.height);
+    //coin
+    ctx.drawImage(picCoin, positionCoin[0], positionCoin[1], picCoin.width,picCoin.height);
+    }
 }
+
 
 picEnemyLeft.onload = picEnemyRight.onload = picPlayerLeft.onload = picPlayerRight.onload = picBackground.onload = draw;
 
@@ -107,10 +153,18 @@ document.addEventListener('keydown',(event)=>{
             navPlayer = 'right';
        //     soundStep.play();
         break;
+        case 'ArrowUp':
+            yPlayer-=speedPlayer;
+            soundStep.play();
+        break;
+        case 'ArrowDown':
+            yPlayer+=speedPlayer;
+            soundStep.play();
+        break;
         case 'Enter':
             startGame=true;
-       //     soundStep.play();
-        break;
+       //     soundStep.play();  
+       break;
     }
     draw();
 });
